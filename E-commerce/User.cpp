e22de4d2fea677a3e3cf7bcbe42f43cf2e3efee8
s61973441@gmail.com <s61973441@gmail.com>
2024-03-19 12:17:40 +0200

    Add Inheritance classes

--- E-commerce/User.cpp
+++ E-commerce/User.cpp
@@ -1,4 +1,5 @@
 #include "User.h"
+#include "Order.h"
 #include <string>
 #include <iostream>
 
@@ -6,7 +7,14 @@ using namespace std;
 
 
 User::User(const string& new_name, const string& new_email, int new_age)
-    : name_u(new_name), email(new_email), age(new_age) {}
+    : name_u(new_name), email(new_email), age(new_age) {
+    cout << " User constructor" << endl;
+}
+
+
+User::User(const User& other) 
+: name_u(other.name_u), email(other.email), age(other.age) {}
+
 
 string User::getName() const {
     return name_u;
@@ -20,10 +28,44 @@ int User::getAge() const {
     return age;
 }
 
-
-
 ostream& operator<<(ostream& os, const User& obj)
 {
     os << "User name: " << obj.name_u << endl << "User email: " << obj.email << endl << "User age: " << obj.age << endl;
     return os;
+}
+
+  
+EmployeeUser::EmployeeUser(const string& new_name, const string& new_email, int age, const string& position, double salary, Order order)
+    : User(new_name, new_email, age), position(position), salary(salary), order(order) {
+    cout << " Employee constructor" << endl;
+}
+
+
+string EmployeeUser::getPosition() const {
+    return position;
+}
+
+
+double EmployeeUser::getSalary() const {
+    return salary;
+}
+
+EmployeeUser::EmployeeUser(const EmployeeUser& other) 
+    : User(other), position(other.position), salary(other.salary), order(other.order) {}
+
+AdminUser::AdminUser(const string& new_name, const string& new_email, int age, const string& position, double salary, Order order , int permission_level, const string& admin_id)
+    : EmployeeUser(new_name, new_email, age, position, salary, order ), permission_level(permission_level), admin_id(admin_id) {
+    cout << " Admin constructor" << endl;
+}
+
+
+
+int AdminUser::getPermissionLevel() const {
+    return permission_level;
+}
+
+
+
+string AdminUser::getAdminId() const {
+    return admin_id;
 }
--- E-commerce/User.h
+++ E-commerce/User.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include "Order.h"
 
 using namespace std;
 
@@ -16,8 +17,41 @@ private:
 
 public:
     User(const string& new_name, const string& new_email, int age);
+    User(const User& other);
     string getName() const;
     string getEmail() const;
     int getAge() const;
+    ~User() { cout << " User destructor" << endl; };
 };
 
+
+
+class EmployeeUser : public User
+{
+private:
+    string position;
+    double salary;
+    Order order;
+public:
+    EmployeeUser(const string& new_name, const string& new_email, int age, const string& position, double salary, Order order);
+    EmployeeUser(const EmployeeUser& other);
+    string getPosition() const;
+    double getSalary() const;
+    ~EmployeeUser() { cout << " Employee destructor" << endl; };
+};
+
+
+
+class AdminUser : public EmployeeUser
+{
+private:
+    string admin_id;
+    int permission_level;
+public:
+    AdminUser(const string& new_name, const string& new_email, int age, const string& position,
+        double salary,Order order, int permission_level, const string& admin_id);
+    ~AdminUser() { cout << " Admin destructor" << endl; };
+    int getPermissionLevel() const;
+    string getAdminId() const;
+
+};
